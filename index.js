0d69eec0101427c6bba57018986f6da1e5330332
Mohdaman01 <mohdastar200026@gmail.com>
2023-04-08 01:00:47 +0530

    first commit: created the directory structure and setup the server.

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+const port  = 3000;
+
+app.listen(port,(err)=>{
+    if(err){
+        console.log(`error: ${err}`);
+    }
+    console.log(`sever is running on http://localhost:${port}`);
+})
