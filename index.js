4c34a9581a34a5f290dcb7a4ce5ee6095c7e9764
Mohdaman01 <mohdastar200026@gmail.com>
2023-04-08 01:20:54 +0530

    second commit: set up the router.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-
 const app = express();
-
 const port  = 3000;
+
+app.use('/',require('./routes'));
 
 app.listen(port,(err)=>{
     if(err){
---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const Router = express.Router();
+
+console.log(`Router loaded`);
+
+module.exports = Router;
