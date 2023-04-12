60374a4749f39bcc60e94dd8f77ed1dc74b8af17
Mohdaman01 <mohdastar200026@gmail.com>
2023-04-12 15:26:13 +0530

    setup changes

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
 const port  = 3000;
+
+app.use(expressLayouts);
+app.use('/',require('./routes'));
+app.use(express.static('./assets'));
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
-
-app.use('/',require('./routes'));
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 
 app.listen(port,(err)=>{
     if(err){
