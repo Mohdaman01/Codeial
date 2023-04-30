56a73c434c1f8add81990993280cb386188a3df1
Mohdaman01 <mohdastar200026@gmail.com>
2023-05-01 00:46:30 +0530

    added upload profile picture.

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,110 +1,137 @@
 const User = require('../models/users');
-const Post =  require('../models/post');
+const Post = require('../models/post');
 const Comment = require('../models/comment');
+const fs = require('fs');
+const path = require('path');
 
-module.exports = function(req,res){
+module.exports = function (req, res) {
     return res.render('user');
 }
 
-module.exports.signin = function(req,res){
-    if(req.isAuthenticated()){
+module.exports.signin = function (req, res) {
+    if (req.isAuthenticated()) {
         return res.redirect('/users/profile');
     }
     return res.render('user_signin');
-}
+}   
 
-module.exports.signup = function(req,res){
-    if(req.isAuthenticated()){
+module.exports.signup = function (req, res) {
+    if (req.isAuthenticated()) {
         return res.redirect('/users/profile');
-    }  
+    }
     return res.render('user_signup');
 }
 
-module.exports.create_user =  async function(req,res){
+module.exports.create_user = async function (req, res) {
     console.log(req.body);
-     if(req.body.passward != req.body.confirm_password){
+    if (req.body.passward != req.body.confirm_password) {
         // console.log('passward do not match');
-        req.flash('error','passwards do not match!')
+        req.flash('error', 'passwards do not match!')
         return res.redirect('back');
 
-     }else{ 
+    } else {
 
-        try{
+        try {
 
-            const user = await User.findOne({email:req.body.email});
+            const user = await User.findOne({ email: req.body.email });
 
-            if(!user){
-                await User.create(req.body); 
-                req.flash('success','Account created successfully!');
+            if (!user) {
+                await User.create(req.body);
+                req.flash('success', 'Account created successfully!');
                 return res.redirect('/users/user-signin');
 
-            }else{
+            } else {
                 // console.log('error this email is all ready registerd!!!');
-                req.flash('error','This email is all ready registerd!');
+                req.flash('error', 'This email is all ready registerd!');
                 return res.redirect('back');
             }
-        }catch(error){
-            console.log('error: ',error);
-            req.flash('error',error);
+        } catch (error) {
+            console.log('error: ', error);
+            req.flash('error', error);
             return res.redirect('back');
         }
 
-     }    
+    }
 }
 
-module.exports.create_session = async function(req,res){
-    req.flash('success','Logged in Successfully!');
+module.exports.create_session = async function (req, res) {
+    req.flash('success', 'Logged in Successfully!');
     return res.redirect('/');
 }
 
-module.exports.profile = async function(req,res){
+module.exports.profile = async function (req, res) {
     const user = await User.findById(req.params.id);
-    return res.render('profile',{
-        title:"profile",
-        profile_user:user
+    return res.render('profile', {
+        title: "profile",
+        profile_user: user
     })
 }
 
-module.exports.update = async function(req,res){
-    if(req.user.id == req.params.id){
-        await User.findByIdAndUpdate(req.params.id,req.body);
-        req.flash('success','Profile updated Successfully!')
-        return res.redirect('back');
-    }else{
-        req.flash('error','You are Unauthorized');
+module.exports.update = async function (req, res) {
+    if (req.user.id == req.params.id) {
+        // await User.findByIdAndUpdate(req.params.id,req.body);
+        try {
+            let user = await User.findById(req.params.id);
+            User.uploadedAvatar(req, res, function(err){
+                if (err) {console.log('*****Multer Error: ', err)}
+                
+                user.name = req.body.name;
+                user.email = req.body.email;
+
+                if (req.file){
+
+                    if (user.avatar){
+                        fs.unlinkSync(path.join(__dirname, '..', user.avatar));
+                    }
+
+
+                    // this is saving the path of the uploaded file into the avatar field in the user
+                    user.avatar = User.avatarPath + '/' + req.file.filename;
+                }
+                user.save();
+                return res.redirect('back');
+            });
+        } catch (err) {
+            console.log('Error: ',err);
+            return res.redirect('back');
+        }
+
+
+    } else {
+        req.flash('error', 'You are Unauthorized');
         return res.status(401).send('Unauthorized');
     }
 }
 
-module.exports.destroy_user = async function(req,res){
-    if(req.user.id == req.params.id){
-        
-        await Post.findOneAndDelete({user:req.params.id});
-        await Comment.deleteMany({user: req.params.id})
+module.exports.destroy_user = async function (req, res) {
+    if (req.user.id == req.params.id) {
+
+        await Post.findOneAndDelete({ user: req.params.id });
+        await Comment.deleteMany({ user: req.params.id })
         await User.findByIdAndDelete(req.params.id);
-        
-        req.logout(function(err) {
+
+        req.logout(function (err) {
             if (err) {
-              return next(err);
+                return next(err);
             }
             return res.redirect("/");
-          });
-        req.flash('success','Account deleted Successfully!');
+        });
+        req.flash('success', 'Account deleted Successfully!');
         return res.redirect('/');
-    }else{
+    } else {
         return res.status(401).send('Unauthorized');
     }
 }
- 
-module.exports.destroySession = function(req,res,next){
-    
-    req.logout(function(err) {
+
+module.exports.destroySession = function (req, res, next) {
+
+    req.logout(function (err) {
 
         if (err) {
-          return next(err);
+            return next(err);
         }
-        
-        req.flash('success','Logged out Succesfully!');
+
+        req.flash('success', 'Logged out Succesfully!');
         return res.redirect("/");
-      });
+    });
 }
--- models/users.js
+++ models/users.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const multer = require('multer');
+const path = require('path');
+const AVATAR_PATH = path.join('/uploads/users/avatars');
+
 
 const userSchema = mongoose.Schema({
     email:{
@@ -13,7 +17,26 @@ const userSchema = mongoose.Schema({
     name:{ 
         type:String,
         required:true
+    },
+    avatar:{
+        type:String
+    }
+},{
+    timestamps:true
+});
+
+let storage = multer.diskStorage({
+    destination: function(req,file,cb){
+        cb(null,path.join(__dirname,'..',AVATAR_PATH));
+    },
+    filename: function(req,file,cb){
+        cb(null,file.fieldname + '-' + Date.now());
     }
 });
 
-module.exports = mongoose.model('user',userSchema);
+userSchema.statics.uploadedAvatar = multer({storage : storage}).single('avatar');
+userSchema.statics.avatarPath = AVATAR_PATH;
+
+const User = mongoose.model('user',userSchema);
+
+module.exports = User;
